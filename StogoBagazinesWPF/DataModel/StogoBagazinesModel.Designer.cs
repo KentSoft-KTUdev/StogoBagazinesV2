0ce424722ee6c5ce4b09028de35c562ed67be5d6
Edvardas-Arnoldas Alaburda <edvardas.alaburda@ktu.edu>
2018-04-22 17:54:40 +0300

    +updated database diagram

--- StogoBagazinesWPF/DataModel/StogoBagazinesModel.Designer.cs
+++ StogoBagazinesWPF/DataModel/StogoBagazinesModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\edvala\source\repos\StogoBagazinesWPF\StogoBagazinesWPF\DataModel\StogoBagazinesModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\edvala\Documents\StogoBagazinesV2\StogoBagazinesWPF\DataModel\StogoBagazinesModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
